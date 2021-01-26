e6b7b8832a4da4fcfb3314eb8c41047699049b4f
Matt <mattfsewell@gmail.com>
2021-01-26 12:56:02 -0500

    commit tree bdb7198f9bfc4819c23b6466698a53d2bd018ad3

---
+++ app/models/show.rb
@@ -0,0 +1,27 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        Show.maximum(:rating)
+    end
+    def self.most_popular_show
+        Show.find_by(rating: self.highest_rating)
+    end
+    def self.lowest_rating
+        Show.minimum(:rating)
+    end
+    def self.least_popular_show
+        Show.find_by(rating: self.lowest_rating)
+    end
+    def self.ratings_sum
+        Show.sum(:rating)
+    end
+    def self.popular_shows
+        Show.all.select {|show| show.rating > 5}
+    end
+    def self.shows_by_alphabetical_order
+        Show.all.select {|show| show.name}.sort! do |a, b| 
+        #    binding.pry
+            a.name <=> b.name
+        end
+    end
+end
